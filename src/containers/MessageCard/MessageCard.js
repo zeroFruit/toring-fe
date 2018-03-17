4d39cd12c4bed99be36c1270cf503974af25aed0
zerofruit <14wnrkim@gmail.com>
2018-03-17 14:48:16 +0900

    css MessageCard

--- src/containers/MessageCard/MessageCard.js
+++ src/containers/MessageCard/MessageCard.js
@@ -9,7 +9,14 @@ class MessageCard extends PureComponent {
     render() {
         return (
             <div className={cx('message-card')}>
-                MessageCard
+                <h1>작가이름</h1>
+                <h2>그림책 제목(가제)</h2>
+                <div className={cx('date')}>
+                    2018.02.28 05:04 p.m.
+                </div>
+                <div className={cx('content')}>
+                    <p>작업 요청이 도착했습니다.<br/>쪽지를 확인하고 그림책을 완성해보세요.</p>
+                </div>
             </div>
         );
     }
